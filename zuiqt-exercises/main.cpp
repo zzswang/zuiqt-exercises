e7c3c495c43235e172237bfd2469b45a7daab2ef
zzswang <zzswang@gmail.com>
2017-01-09 18:16:34 +0800

    Initial Commit

---
+++ zuiqt-exercises/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zuiqt-exercises
+//
+//  Created by 王俊华 on 2017/1/9.
+//  Copyright © 2017年 zzs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
